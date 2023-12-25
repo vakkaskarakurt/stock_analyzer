c213df1922143528f7a7ffe016fbb5bb81049d45
vakkaskarakurt <karakurtvakkas@gmail.com>
2023-12-25 23:41:43 +0300

    Updated for interval=30 minutes

--- stock_analyzer.py
+++ stock_analyzer.py
@@ -5,43 +5,50 @@ import pandas as pd
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 import tkinter as tk
+from tkinter import ttk
 from custom_exceptions import StockAnalyzerError
 
 class StockAnalyzer:
+    MINUTE_INTERVAL = "30m"
+    DAY_INTERVAL = "1d"
+    CURRENCY_CODE = "TRYUSD=X"
+    TIME_RANGES = {
+        '1 Hafta': timedelta(days=7),
+        '1 Ay': timedelta(days=30),
+        '3 Ay': timedelta(days=90),
+        '6 Ay': timedelta(days=180),
+        '1 Yıl': timedelta(days=365),
+        '3 Yıl': timedelta(days=365*3),
+        '5 Yıl': timedelta(days=365*5),
+        '10 Yıl': timedelta(days=365*10)
+    }
+
     @staticmethod
-    def get_price_data(stock_code, currency_code, start_date, end_date):
-        """
-        Belirtilen hisse senedi ve döviz kuru için fiyat verilerini alır ve hesaplar.
-        """
+    def get_price_data(stock_code, currency_code, start_date, end_date, time_range):
         try:
-            stock_data = yf.download(stock_code, start_date, end_date)
-            currency_data = yf.download(currency_code, start_date, end_date)
+            time_range_timedelta = StockAnalyzer.TIME_RANGES[time_range]
+            if time_range_timedelta <= timedelta(days=30):
+                stock_data = yf.download(stock_code, start=start_date, end=end_date, interval=StockAnalyzer.MINUTE_INTERVAL)
+                currency_data = yf.download(currency_code, start=start_date, end=end_date, interval=StockAnalyzer.MINUTE_INTERVAL)
+            else:
+                stock_data = yf.download(stock_code, start=start_date, end=end_date, interval=StockAnalyzer.DAY_INTERVAL)
+                currency_data = yf.download(currency_code, start=start_date, end=end_date, interval=StockAnalyzer.DAY_INTERVAL)
+
+            # Tarih indekslerini aynı zaman dilimine dönüştürme
+            stock_data.index = stock_data.index.tz_localize(None)
+            currency_data.index = currency_data.index.tz_localize(None)
+
+            # Veri çerçevelerini tarih sütununa göre birleştirme
             merged_data = pd.merge(stock_data['Close'], currency_data['Close'], left_index=True, right_index=True, suffixes=(' (' + stock_code + '/TL)', ' (TL/USD)'))
             merged_data['Close (USD)'] = merged_data['Close (' + stock_code + '/TL)'] * merged_data['Close (TL/USD)']
             return merged_data
-        except yf.YFinanceError as e:
-            raise StockAnalyzerError(f"YFinanceError: {e}")
+
         except Exception as e:
             raise StockAnalyzerError(f"An error occurred: {e}")
 
     @staticmethod
     def determine_time_range(end_date, time_range):
-        if time_range == '1 Hafta':
-            return end_date - timedelta(days=7)
-        elif time_range == '1 Ay':
-            return end_date - timedelta(days=30)
-        elif time_range == '3 Ay':
-            return end_date - timedelta(days=90)
-        elif time_range == '6 Ay':
-            return end_date - timedelta(days=180)
-        elif time_range == '1 Yıl':
-            return end_date - timedelta(days=365)
-        elif time_range == '3 Yıl':
-            return end_date - timedelta(days=365*3)
-        elif time_range == '5 Yıl':
-            return end_date - timedelta(days=365*5)
-        elif time_range == '10 Yıl':
-            return end_date - timedelta(days=365*10)
+        return end_date - StockAnalyzer.TIME_RANGES[time_range]
 
     @staticmethod
     def time_range_button_clicked(stock_entry, result_text, chart_canvas, progress_bar, time_range):
@@ -50,15 +57,17 @@ class StockAnalyzer:
 
         try:
             progress_bar.start()
-            end_date = datetime.now()
-            start_date = StockAnalyzer.determine_time_range(end_date, time_range)
+            end_date = datetime.now().strftime("%Y-%m-%d")
+            start_date = StockAnalyzer.determine_time_range(datetime.now(), time_range)
             stock_code = stock_entry.get() + ".IS"
-            currency_code = "TRYUSD=X"
-            result_data = StockAnalyzer.get_price_data(stock_code, currency_code, start_date, end_date)
+            currency_code = StockAnalyzer.CURRENCY_CODE
+            result_data = StockAnalyzer.get_price_data(stock_code, currency_code, start_date, end_date, time_range)
             StockAnalyzer.update_result_text(result_text, result_data)
             StockAnalyzer.plot_chart(result_data, chart_canvas)
+
         except StockAnalyzerError as e:
             raise StockAnalyzerError(f"An error occurred: {e}")
+
         finally:
             progress_bar.stop()
 
@@ -80,3 +89,5 @@ class StockAnalyzer:
         plt.legend()
         plt.grid(True, linestyle='--', alpha=0.7)
         chart_canvas.draw()
+
+# Additional import statements and GUI-related code go here
