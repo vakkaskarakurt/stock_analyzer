84b5653118985232d33c5645d5948c53553dc645
vakkaskarakurt <karakurtvakkas@gmail.com>
2023-12-25 23:25:52 +0300

    .

--- txtolustur.py
+++ txtolustur.py
@@ -1,12 +1,12 @@
 def dosya_icerigini_txtye_yaz(py_dosyalari, txt_dosyasi):
-    with open(txt_dosyasi, 'w') as hedef_dosya:
+    with open(txt_dosyasi, 'w', encoding='utf-8') as hedef_dosya:
         for py_dosyasi in py_dosyalari:
-            with open(py_dosyasi, 'r') as kaynak_dosya:
+            with open(py_dosyasi, 'r', encoding='utf-8') as kaynak_dosya:
                 hedef_dosya.write(f"\n\n# {py_dosyasi}\n\n")
                 hedef_dosya.write(kaynak_dosya.read())
 
 # Kullanım örneği:
-py_dosyalar = ["main.py", "gui_manager.py", "stock_analyzer.py","custom_exceptions.py"]  # İşlem yapılacak .py dosyalarının listesi
-txt_dosyasi = "sonuc.txt"  # İçeriklerin yazılacağı txt dosyasının adı
+py_dosyalar = ["main.py", "gui_manager.py", "stock_analyzer.py", "custom_exceptions.py"]
+txt_dosyasi = "sonuc.txt"
 
 dosya_icerigini_txtye_yaz(py_dosyalar, txt_dosyasi)
