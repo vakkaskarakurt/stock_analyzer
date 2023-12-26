cda15a71d2d17b12197f61d2c53d55d6345db464
vakkaskarakurt <karakurtvakkas@gmail.com>
2023-12-26 12:00:06 +0300

    .

--- deneme.py
+++ deneme.py
@@ -1,28 +1,38 @@
 import yfinance as yf
-from datetime import datetime
+from datetime import datetime, timedelta
 import pandas as pd
+import matplotlib.pyplot as plt
 
 stock_code = "HEKTS.IS"
-start_date = "2023-11-14"
-end_date = datetime.now().strftime("%Y-%m-%d")
-end_date = "2023-11-15"
+start_date = "2023-12-20"
+end_date = "2023-12-26"
 
 currency_code = "TRYUSD=X"
 
-# Hisse senedi ve döviz kuru verilerini yfinance kullanarak çek
-stock_data = yf.download(stock_code, start=start_date, end=end_date, interval="30m")
-currency_data = yf.download(currency_code, start=start_date, end=end_date, interval="1h")
+# Hisse senedi ve döviz verilerini indir
+stock_data = yf.download(stock_code, start=start_date, end=end_date, interval="30m")["Close"]
+currency_data = yf.download(currency_code, start=start_date, end=end_date, interval="30m")["Close"]
 
-
-print(stock_data)
-print(currency_data)
-
-# Tarih indekslerini aynı zaman dilimine dönüştürme
-stock_data.index = stock_data.index.tz_localize(None)
-currency_data.index = currency_data.index.tz_localize(None)
-
-# Veri çerçevelerini tarih sütununa göre birleştirme
-merged_data = pd.merge(stock_data, currency_data, how='inner', left_index=True, right_index=True)
+# Verileri 'outer' birleştir
+merged_data = pd.merge(stock_data, currency_data, left_index=True, right_index=True)
 merged_data["Close (USD)"] = merged_data["Close_x"] * merged_data["Close_y"]
 
-print(merged_data["Close (USD)"])
+# Hafta sonlarını sil
+merged_data = merged_data[~merged_data.index.dayofweek.isin([5, 6])]  # 5: Cumartesi, 6: Pazar
+
+# Silinmeyen index'lerdeki günleri sil
+merged_data = merged_data.dropna(subset=["Close (USD)"])
+
+# Index'i sıfırla sadece veri olmayanlar için
+merged_data.reset_index(drop=True, inplace=True)
+
+# Çizim yap 
+plt.figure(figsize=(10, 6))
+plt.plot(merged_data.index, merged_data["Close (USD)"], label="Close (USD)")
+plt.title("Stock Closing Price (USD)")
+plt.xlabel("Date")
+plt.ylabel("Price (USD)")
+plt.legend()
+plt.xticks(rotation=45)
+plt.grid(True)
+plt.show()
